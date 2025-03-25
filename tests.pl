18ecb14aa969a42cb9db3636ad5f7e4dc65d6689
AUGUSTRMZ <142357102+AUGUSTRMZ@users.noreply.github.com>
2025-03-25 14:44:26 -0600

    Add files via upload

---
+++ tests.pl
@@ -0,0 +1,17 @@
+?- acepta(['A','d','u','i','a','l']). 
+true.
+
+?- acepta(['A','h','a']). 
+true.
+
+?- acepta(['A','i','n','a']). 
+true.
+
+?- acepta(['A','g','l','a','r','o','n','d']). 
+true.
+
+?- acepta(['A','i']). 
+true.
+
+?- acepta(['A','b','c']). 
+false.
