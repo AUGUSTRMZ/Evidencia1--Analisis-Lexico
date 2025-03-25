649270e8aeefbe8df1f5f5f8d1a8bd1a30e4c9ee
AUGUSTRMZ <142357102+AUGUSTRMZ@users.noreply.github.com>
2025-03-25 14:41:27 -0600

    Add files via upload

--- automata.pl
+++ automata.pl
@@ -7,31 +7,30 @@ transicion(inicial, 'A', q1).
 
 % Aduial
 transicion(q1, 'd', q2).
-transicion(q2, 'u', q3).
-transicion(q3, 'i', q4).
-transicion(q4, 'a', q5).
-transicion(q5, 'l', final).
+transicion(q2, 'u', q6).
+transicion(q6, 'i', q7).
+transicion(q7, 'a', q8).
+transicion(q8, 'l', final).
 
 % Aglarond
-transicion(q1, 'g', q6).
-transicion(q6, 'l', q7).
-transicion(q7, 'a', q8).
-transicion(q8, 'r', q9).
-transicion(q9, 'o', q10).
-transicion(q10, 'n', q11).
-transicion(q11, 'd', final).
+transicion(q1, 'g', q3).
+transicion(q3, 'l', q10).
+transicion(q10, 'a', q11).
+transicion(q11, 'r', q12).
+transicion(q12, 'o', q13).
+transicion(q13, 'n', q14).
+transicion(q14, 'd', final).
 
 % Aha
-transicion(q1, 'h', q20).
-transicion(q20, 'a', final).
+transicion(q1, 'h', q4).
+transicion(q4, 'a', final).
 
 % Ai
-transicion(q1, 'i', q12).
-transicion(q12, 'a', final).
+transicion(q1, 'i',q5).
 
 % Aina
-transicion(q12, 'n', q13).
-transicion(q13, 'a', final).
+transicion(q5, 'n', q18).
+transicion(q18, 'a', final).
 
 % estados de aceptacion
 
